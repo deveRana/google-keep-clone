1ed5ca48642c085ec4d59fbb7d1a8920d1c10b7e
deveRana <developerrana0509@gmail.com>
2023-04-03 21:17:16 +0530

    ready to deploy

---
+++ src/components/NavList.jsx
@@ -0,0 +1,33 @@
+
+
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { LightbulbOutlined as Lightbulb, ArchiveOutlined as Archive, DeleteOutlineOutlined as Delete } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+
+const NavList = () => {
+
+    const navList = [
+        { id: 1, name: 'Notes', icon: <Lightbulb />, route: '/' },
+        { id: 2, name: 'Archives', icon: <Archive />, route: '/archive' },
+        { id: 3, name: 'Trash', icon: <Delete />, route: '/delete' },
+    ]
+
+    return (
+        <List>
+            {
+                navList.map(list => (
+                    <Link to={`${list.route}`} style={{ textDecoration: 'none', display: 'flex', color: 'inherit' }}>
+                        <ListItem button key={list.id}>
+                            <ListItemIcon style={{ alignItems: 'center' }}>
+                                {list.icon}
+                            </ListItemIcon>
+                            <ListItemText primary={list.name} />
+                        </ListItem>
+                    </Link>
+                ))
+            }
+        </List>
+    )
+}
+
+export default NavList;
